31896ad7d74a092f4338da29cd1cd6ca7d80013c
sologuboved <sologuboved@gmail.com>
2018-01-27 00:48:29 +0300

    renames

--- scraper.py
+++ scraper.py
@@ -1,32 +1,58 @@
-import time
-import selenium.webdriver as webdriver
-from selenium.webdriver.common.keys import Keys
-from random import randrange
+import re
+import requests
+from bs4 import BeautifulSoup
+from json_operations import *
+from process_txt import collect_urls
 
 
-def scroll(url, lim):
-    driver = webdriver.Firefox()
-    # driver = webdriver.Chrome()
-    driver.get(url)
-    body = driver.find_element_by_tag_name('body')
+def dump_collection(json_fname, fname25, fname, prefix):
+    print("Dumping json...")
+    dump_json(build_collection(fname25, fname, prefix), json_fname)
 
-    load_button = body.find_element_by_xpath('//a[contains(@class, "_1cr2e _epyes")]')
-    body.send_keys(Keys.END)
-    time.sleep(3)
 
-    load_button.click()
-
-    ind = 0
-    while ind <= lim:
-        print(ind)
+def build_collection(fname25, fname, prefix):
+    print("Building collection...")
+    collection = dict()
+    urls = collect_urls(fname25, fname, prefix)
+    total = len(urls)
+    ind = 1
+    for url in urls:
+        print("(%d of %d) %s" % (ind, total, url))
+        collection[url] = scrape_page(url)
         ind += 1
-        body.send_keys(Keys.END)
-        time.sleep(randrange(2, 5))
+    return collection
 
-    print(driver.page_source)
+
+def scrape_page(page_url):
+    page_html = requests.get(page_url).content
+    str_soup = str(BeautifulSoup(page_html, 'lxml'))
+
+    # print(str_soup)
+
+    # <meta content="0 Likes, 1 Comments - Буратино (@thalassografia) on Instagram: “Собр. статей греческих
+    # психоаналитиков о У.Р. Бионе #βιβλία #w_r_bion #ελληνικά_τώρα”" name="description"/>
+
+    # <meta content="dusk" property="instapp:hashtags"/>
+
+    # <meta content="Instagram post by Буратино • Jan 7, 2018 at 10:17am UTC" property="og:title"/>
+
+    try:
+        likes = re.findall('<meta content="(.*?) Likes', str_soup)[0]
+    except IndexError:
+        likes = None
+        print()
+        print("Likes rubric missing:")
+        print(page_url)
+        print()
+    tags = re.findall('<meta content="(.*?)" property="instapp:hashtags"/>', str_soup)
+    date = re.findall('<meta content="Instagram post by Буратино • (.*?) at', str_soup)
+    return {'likes': likes, 'tags': tags, 'date': date}
 
 
 if __name__ == '__main__':
-    scroll('https://www.instagram.com/thalassografia/', 10)
-
-
+    pass
+    # scrape_page('https://www.instagram.com/p/BdpQMUFBf3b/?taken-by=thalassografia')
+    # dump_collection('mydata.json', 'myrawdata25.txt', 'myrawdata.txt', 'https://www.instagram.com')
+    coll = load_json('mydata.json')
+    print(coll['https://www.instagram.com/p/7TNru4AwSX/?taken-by=thalassografia'])
+    # print(len(coll))
