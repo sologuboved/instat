caf376de25981691a67319eec9194540ec3d4bf3
sologuboved <sologuboved@gmail.com>
2018-01-27 14:50:20 +0300

    scraped times in addition to dates

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ from process_txt import collect_urls
 
 MYDATA = 'myrawdata.txt'
 MYDATA25 = 'myrawdata25.txt'
+INSTAURLS = 'instaurls.json'
 MYDATA_JSON = 'mydata.json'
 INSTAGRAM = 'https://www.instagram.com'
 
@@ -16,14 +17,16 @@ DATE = 'date'
 
 
 def dump_collection(json_fname, fname25, fname, prefix):
-    print("Dumping json...")
     dump_json(build_collection(fname25, fname, prefix), json_fname)
 
 
 def build_collection(fname25, fname, prefix):
-    print("Building collection...")
     collection = dict()
-    urls = collect_urls(fname25, fname, prefix)
+    try:
+        urls = load_json(INSTAURLS)
+    except FileNotFoundError as e:
+        print(e)
+        urls = collect_urls(fname25, fname, prefix)
     total = len(urls)
     ind = 1
     for url in urls:
@@ -48,30 +51,45 @@ def scrape_page(page_url):
 
     try:
         likes = re.findall('<meta content="(.*?) Likes', str_soup)[0]
+        try:
+            likes = int(likes)
+        except (TypeError, ValueError):
+            print_glitch("Likes rubric contains a non-int:", page_url)
     except IndexError:
         likes = None
-        print()
-        print("Likes rubric missing:")
-        print(page_url)
-        print()
+        print_glitch("Likes rubric missing:", page_url)
     tags = re.findall('<meta content="(.*?)" property="instapp:hashtags"/>', str_soup)
-    date = re.findall('<meta content="Instagram post by Буратино • (.*?) at', str_soup)
+    date = re.findall('<meta content="Instagram post by Буратино • (.*?) at (.*?)" property', str_soup)
+    try:
+        date = date[0]
+        date = date[0] + ' ' + date[1]
+    except IndexError:
+        date = None
+        print_glitch("Date rubric missing or disfigured:", page_url)
     return {LIKES: likes, TAGS: tags, DATE: date}
 
 
-def convert(json_fname):
-    collection = load_json(json_fname)
-    for url in collection:
-        item = collection[url]
-        item[LIKES] = int(item[LIKES])
-        item[DATE] = item[DATE][0]
-    dump_json(collection, json_fname)
+def print_glitch(text, page_url):
+    print()
+    print(text)
+    print(page_url)
+    print()
+
+
+# def convert(json_fname):
+#     collection = load_json(json_fname)
+#     for url in collection:
+#         item = collection[url]
+#         item[LIKES] = int(item[LIKES])
+#         item[DATE] = item[DATE][0] + ' ' + item[DATE][1]
+#         print(item[DATE])
+#     # dump_json(collection, json_fname)
 
 
 if __name__ == '__main__':
     pass
     # scrape_page('https://www.instagram.com/p/BdpQMUFBf3b/?taken-by=thalassografia')
-    # dump_collection(MYDATA_JSON, MYDATA25, MYDATA, INSTAGRAM)
+    dump_collection(MYDATA_JSON, MYDATA25, MYDATA, INSTAGRAM)
     # coll = load_json(MYDATA_JSON)
     # print(coll['https://www.instagram.com/p/7TNru4AwSX/?taken-by=thalassografia'])
     # print(len(coll))
