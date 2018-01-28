379ed1b98a08ed418b9e44e0e3397aed858ed60d
sologuboved <sologuboved@gmail.com>
2018-01-28 13:11:28 +0300

    added find_mean, find_variance, find_sd

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,5 @@
 import json
+from math import sqrt
 
 
 def load_json(json_file):
@@ -9,3 +10,15 @@ def load_json(json_file):
 def dump_json(entries, json_file):
     with open(json_file, 'w') as handler:
         json.dump(entries, handler)
+
+
+def find_mean(vals):
+    return sum(vals) / float(len(vals))
+
+
+def find_variance(vals):
+    return sum([(val - find_mean(vals)) ** 2 for val in vals]) / len(vals)
+
+
+def find_sd(vals):
+    return sqrt(find_variance(vals))
