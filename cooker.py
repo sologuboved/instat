1c0e23d0171290ae30a971da5a808dfa7be707e8
sologuboved <sologuboved@gmail.com>
2018-02-01 16:59:51 +0300

    joined together time-analysing functions and fixed minor bugs

--- cooker.py
+++ cooker.py
@@ -17,8 +17,10 @@ class Instat(object):
         self.cook()
 
     def cook(self):
+
         self.collection = list()
         collection = load_json(self.json_fname)
+
         for url in collection:
             item = collection[url]
             tags = item[TAGS]
@@ -27,22 +29,29 @@ class Instat(object):
             date = datetime.strptime(item[DATE], "%b %d, %Y %I:%M%p %Z") + timedelta(hours=3)
             self.collection.append({URL: url, LIKES: item[LIKES], TAGS: tags, DATE: date})
 
+
     def filter_by_date(self, start, end, printer_on=False):
         # filter_by_date('29.01.2017', '29.01.2018')
+
         try:
             start, end = map(lambda d: datetime.strptime(d, "%d.%m.%Y"), (start, end))
         except ValueError:
             print("Wrong date(s)")
             return
+
         self.collection = [item for item in self.collection if start <= item[DATE] < end]
+
         if printer_on:
             print("Filtered from", start, 'to', end)
             self.prettyprint_sorted_by(DATE, large_to_small=False)
 
     def prettyprint(self, allotment=None):
+
         ind = 1
+
         if not allotment:
             allotment = self.collection
+
         for item in allotment:
             print('<%d>' % ind)
             print('url:', item[URL])
@@ -54,46 +63,56 @@ class Instat(object):
             print()
             ind += 1
 
+        print()
+
     def prettyprint_sorted_by(self, field, large_to_small=True, allotment=None):
+
         if not allotment:
             allotment = self.collection
+
         allotment.sort(key=lambda i: i[field], reverse=large_to_small)
         self.prettyprint(allotment)
 
-    def get_random_pic(self):
-        print(self.collection[random.randrange(len(self.collection))][URL])
+    def get_random_pic(self): print(self.collection[random.randrange(len(self.collection))][URL], '\n')
 
-    def find_by_tag(self, tag):
-        self.prettyprint([item for item in self.collection if tag in item[TAGS]])
+    def find_by_tag(self, tag): self.prettyprint([item for item in self.collection if tag in item[TAGS]])
 
-    def find_total_likes(self):
-        print(sum(item[LIKES] for item in self.collection))
+    def find_total_likes(self): print(sum(item[LIKES] for item in self.collection), '\n')
 
     def count_by_year(self):
+
         prefix = '01.01.'
-        self.filter_by_date("1.01.1900", "1.01.2018")
+        self.collection.sort(key=lambda i: i[DATE])
         initial_year = int(self.collection[0][DATE].strftime('%Y'))
         curr_year = int(datetime.today().strftime('%Y'))
         from_year = initial_year
         till_year = initial_year + 1
+
         while till_year <= curr_year + 1:
             self.cook()
             self.filter_by_date(prefix + str(from_year), prefix + str(till_year))
             print("%d: %d pics" % (from_year, len(self.collection)))
             from_year += 1
             till_year += 1
+
         self.cook()
-        print(len(self.collection), "pics total")
+
+        print(len(self.collection), "pics total", '\n')
 
     def find_tag_freqs(self):
+
         tag_freqs = dict.fromkeys(list(self.tags), 0)
+
         for item in self.collection:
             for tag in item[TAGS]:
                 tag_freqs[tag] += 1
+
         for freq in sorted(tag_freqs.items(), key=lambda i: i[1], reverse=True):
             print(freq[0] + ':', freq[1])
+        print()
 
     def analyse_tags(self, larger_than=0, smaller_than=float('inf'), sort_by_mean=False):
+
         all_tags = dict()
         for item in self.collection:
             tags = item[TAGS]
@@ -101,23 +120,26 @@ class Instat(object):
                 likes = all_tags.get(tag, list())
                 likes.append(item[LIKES])
                 all_tags[tag] = likes
+
         filtered_tags = list()
         for tag in all_tags:
             likes = all_tags[tag]
-            times = len(likes)
-            if larger_than < times < smaller_than:
+            num_likes = len(likes)
+            if larger_than < num_likes < smaller_than:
                 total_likes = sum(likes)
-                mean = total_likes / float(times)
+                mean = total_likes / float(num_likes)
                 s_d = find_sd(likes, mean)
                 try:
                     quotient = mean / s_d
-                    filtered_tags.append((tag, times, total_likes, mean, s_d, quotient))
+                    filtered_tags.append((tag, num_likes, total_likes, mean, s_d, quotient))
                 except ZeroDivisionError:
                     pass
+
         if sort_by_mean:
             filtered_tags.sort(key=lambda i: i[3], reverse=True)
         else:
             filtered_tags.sort(key=lambda i: i[5], reverse=True)
+
         for item in filtered_tags:
             tag = item[0]
             w_s = ' ' * (20 - len(tag))
@@ -127,45 +149,47 @@ class Instat(object):
                                                                                           item[4],
                                                                                           item[1],
                                                                                           item[2]))
+        print()
 
-    def find_dayofweek_stat(self):
-        dow = dict()
-        for item in self.collection:
-            day = item[DATE].strftime('%a')
-            times_likes = dow.get(day, [0, 0])
-            times_likes[0] += 1
-            times_likes[1] += item[LIKES]
-            dow[day] = times_likes
-        for day in dow:
-            times, likes = dow[day]
-            print("%s: μ = %f, posted %d times, received %d likes" % (day, float(likes) / times, times, likes))
+    def analyse_time(self, by_day):
 
-    def find_timeofday_stat(self):
-        tod = dict()
+        def sort_days(element): return element[1][2]
+
+        def sort_hours(element): return element[0]
+
+        if by_day:
+            timestring = '%a'
+            sorter = sort_days
+        else:
+            timestring = '%H'
+            sorter = sort_hours
+
+        times = dict()
+
         for item in self.collection:
-            hour = item[DATE].strftime('%H')
-            likes = tod.get(hour, list())
+            timelet = item[DATE].strftime(timestring)
+            likes = times.get(timelet, list())
             likes.append(item[LIKES])
-            tod[hour] = likes
-        for hour in tod:
-            likes = tod[hour]
-            times = len(likes)
+            times[timelet] = likes
+
+        for timelet in times:
+            likes = times[timelet]
+            num_likes = len(likes)
             total_likes = sum(likes)
-            mean = float(total_likes) / times
+            mean = float(total_likes) / num_likes
             s_d = find_sd(likes, mean)
-            tod[hour] = (times, total_likes, mean, s_d)
-        for item in sorted(tod.items(), key=lambda i: i[0]):
-            hour, vals = item
-            w_s = ' ' * (5 - len(hour))
-            print("%s:%s μ = %f, σ = %f, posted %d times, received %d likes" % (hour, w_s,
+            times[timelet] = (num_likes, total_likes, mean, s_d)
+        for item in sorted(times.items(), key=sorter):
+            timelet, vals = item
+            w_s = ' ' * (5 - len(timelet))
+            print("%s:%s μ = %f, σ = %f, posted %d times, received %d likes" % (timelet, w_s,
                                                                                 vals[2], vals[3], vals[0], vals[1]))
+        print()
 
 
 if __name__ == '__main__':
     inst = Instat(MYDATA_JSON)
+    inst.count_by_year()
     inst.filter_by_date('29.01.2014', '29.01.2019', printer_on=False)
-    print()
-    inst.find_dayofweek_stat()
-    print()
-    inst.find_timeofday_stat()
-
+    inst.analyse_time(by_day=True)
+    inst.analyse_time(by_day=False)
